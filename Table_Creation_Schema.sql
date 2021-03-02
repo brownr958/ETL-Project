3fe597659bf75f564cdfcdeaf21b935bfe81a892
Ryan Brown <brownr958@gmail.com>
2021-03-02 16:06:43 -0500

    updates

--- Table_Creation_Schema.sql
+++ Table_Creation_Schema.sql
@@ -1,27 +1,30 @@
+DROP TABLE Games;
+ 
 CREATE TABLE Publisher (
 	Publisher_ID INT PRIMARY KEY,
-	Publsher_Name VARCHAR);
+	Publisher VARCHAR);
 
 CREATE TABLE Games (
 	Game_ID INT PRIMARY KEY,
-	Game_Name VARCHAR,
+	Game VARCHAR,
 	Year INT,
 	Genre VARCHAR,
-	Publisher INT FOREIGN KEY REFERENCES Publisher(Publisher_ID),
-	Total_Global_Sales INT);
+	Publisher VARCHAR,
+	Total_Global_Sales FLOAT
+	);
 	
 CREATE TABLE PS4_Games (
-	Game_ID INT FOREIGN KEY REFERENCES Games(Game_ID),
-	PS4_NA_Sales INT,
-	PS4_EU_Sales INT,
-	PS4_JP_Sales INT,
-	PS4_RoW_Sales INT,
-	PS4_RoW_Sales INT);
+	Game VARCHAR,
+	PS4_NA_Sales FLOAT,
+	PS4_EU_Sales FLOAT,
+	PS4_JP_Sales FLOAT,
+	PS4_RoW_Sales FLOAT,
+	PS4_Global_Sales FLOAT);
 	
 CREATE TABLE Xbox_Games (
-	Game_ID INT FOREIGN KEY REFERENCES Games(Game_ID),
-	Xbox_NA_Sales INT,
-	Xbox_EU_Sales INT,
-	Xbox_JP_Sales INT,
-	Xbox_RoW_Sales INT,
-	Xbox_RoW_Sales INT);
+	Game VARCHAR,
+	Xbox_NA_Sales FLOAT,
+	Xbox_EU_Sales FLOAT,
+	Xbox_JP_Sales FLOAT,
+	Xbox_RoW_Sales FLOAT,
+	Xbox_Global_Sales FLOAT);
